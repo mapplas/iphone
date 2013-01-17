ba6662c25dd4ad9c9db8cf5d7c86da313ce30183
belen-mapplas <belen@mapplas.com>
2013-01-17 10:13:02 +0100

    Created Mapplas project for iPhone.

---
+++ Mapplas/Mapplas/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Mapplas
+//
+//  Created by Belén  on 17/01/13.
+//  Copyright (c) 2013 Mapplas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Mapplas/MapplasTests/MapplasTests.m
@@ -0,0 +1,32 @@
+//
+//  MapplasTests.m
+//  MapplasTests
+//
+//  Created by Belén  on 17/01/13.
+//  Copyright (c) 2013 Mapplas. All rights reserved.
+//
+
+#import "MapplasTests.h"
+
+@implementation MapplasTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MapplasTests");
+}
+
+@end
