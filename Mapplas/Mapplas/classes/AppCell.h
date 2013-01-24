f8f6e16b6eab65a5fa3cef29b8a828c4301f1b34
belen-mapplas <belen@mapplas.com>
2013-01-24 11:26:14 +0100

    Created base applications cell.

---
+++ Mapplas/Mapplas/classes/AppCell.h
@@ -0,0 +1,16 @@
+//
+//  AppCell.h
+//  Mapplas
+//
+//  Created by Belén  on 24/01/13.
+//  Copyright (c) 2013 Mapplas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppCell : UITableViewCell
+
+@property (nonatomic, strong) IBOutlet UIImageView *imageLogo;
+@property (nonatomic, strong) IBOutlet UIImageView *imageRoundView;
+
+@end
---
+++ Mapplas/Mapplas/classes/AppCell.m
@@ -0,0 +1,30 @@
+//
+//  AppCell.m
+//  Mapplas
+//
+//  Created by Belén  on 24/01/13.
+//  Copyright (c) 2013 Mapplas. All rights reserved.
+//
+
+#import "AppCell.h"
+
+@implementation AppCell
+
+@synthesize imageLogo;
+@synthesize imageRoundView;
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
